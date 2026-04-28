8124a7737eb22b5092db1e675ae2084acf286829
Utsav Siwakoti <utsavsiwakoti007@gmail.com>
2026-04-28 23:04:45 +0545

    Sync backend sales-and-loyalty branch

--- VehicleParts.Infrastructure/obj/Debug/net9.0/VehicleParts.Infrastructure.AssemblyInfo.cs
+++ VehicleParts.Infrastructure/obj/Debug/net9.0/VehicleParts.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VehicleParts.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bf3ab4da6a237fe001acb77fef6bd1c4ff1d28d")]
 [assembly: System.Reflection.AssemblyProductAttribute("VehicleParts.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VehicleParts.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
